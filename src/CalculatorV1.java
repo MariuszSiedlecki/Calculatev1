92bd1c454fafa0bcd75ca8aafe2797d2c8586bc2
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-11-18 12:49:41 +0100

    c

--- src/CalculatorV1.java
+++ src/CalculatorV1.java
@@ -6,6 +6,8 @@ public class CalculatorV1 {
 
     int numberFromUser1;
 
+    public static final int EXIT = 7;
+
     public static void main(String[] args) {
         CalculatorV1 calculatorv2 = new CalculatorV1();
         calculatorv2.printMenu();
@@ -20,23 +22,25 @@ public class CalculatorV1 {
             System.out.println("4 - Division [/]");
             System.out.println("5 - Exponentiation [^]");
             System.out.println("6 - Factorial [!]");
-            System.out.println("7 - Exit");
+            System.out.println(EXIT + " - Exit");
 
             parseUserChoice(getNumberFromUser());
 
-        } while (true);
+        } while ( true );
     }
 
     private int getNumberFromUser() {
         System.out.println("Your choice:");
         return scanner.nextInt();
     }
-    private int getNumber(){
-        System.out.println("Podaj pierwszą liczbe:");
+
+    private int getNumber() {
+        System.out.println("Enter the first number:");
         return scanner.nextInt();
     }
-    private int getNumberTwo(){
-        System.out.println("Podaj drugą liczbe:");
+
+    private int getNumberTwo() {
+        System.out.println("Enter the second number:");
         return scanner.nextInt();
     }
 
@@ -48,7 +52,7 @@ public class CalculatorV1 {
                 break;
             }
             case 2: {
-                print(substring(getNumber(),getNumberTwo()));
+                print(substring(getNumber(), getNumberTwo()));
                 break;
             }
             case 3: {
@@ -56,21 +60,21 @@ public class CalculatorV1 {
                 break;
             }
             case 4: {
-                print(division(getNumber(),getNumberTwo()));
+                print(division(getNumber(), getNumberTwo()));
                 break;
             }
-            case 5:{
-                print(exponentiation(getNumber(),numberFromUser1));
+            case 5: {
+                print(exponentiation(numberFromUser1));
                 break;
             }
-            case 6:{
+            case 6: {
                 System.out.println("Result: " + factorial());
                 break;
 
             }
             case 7: {
                 System.out.println("The end.");
-                System.exit(0);
+                System.exit(EXIT);
                 break;
             }
             default:
@@ -83,22 +87,26 @@ public class CalculatorV1 {
     private int add(int numberFromUser, int numberFromUser1) {
         return numberFromUser + numberFromUser1;
     }
+
     private int substring(int numberFromUser, int numberFromUser1) {
         return numberFromUser - numberFromUser1;
     }
+
     private int multiply(int numberFromUser, int numberFromUser1) {
         return numberFromUser * numberFromUser1;
     }
+
     private int division(int numberFromUser, int numberFromUser1) {
         return numberFromUser / numberFromUser1;
     }
-    private int exponentiation(int numberFromUser, int numberFromUser1) {
-        System.out.println("To which power ? ");
-        numberFromUser1 = scanner.nextInt();
+
+    private int exponentiation(int numberFromUser) {
+        System.out.println("give the number to the second power:");
+        numberFromUser = scanner.nextInt();
 
         int result = numberFromUser;
 
-        for (int i = 0; i < numberFromUser1; i++) {
+        for (int i = 0; i < numberFromUser; i++) {
             result *= numberFromUser;
             break;
         }
@@ -108,10 +116,13 @@ public class CalculatorV1 {
     private BigInteger factorial() {
         System.out.println("podaj liczbe: ");
         numberFromUser1 = scanner.nextInt();
-        BigInteger sum =  BigInteger.ONE;
+        BigInteger sum = BigInteger.ONE;
 
-        for (int i = 2; i <= numberFromUser1; i++) {
+        for (int i = 1; i <= numberFromUser1; i++) {
             sum = sum.multiply(BigInteger.valueOf(i));
+        }
+        if (numberFromUser1 < 0) {
+            System.out.println("This is not a natural number! Try again...");
         }
         return sum;
     }
